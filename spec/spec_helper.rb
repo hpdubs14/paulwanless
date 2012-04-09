0b862e73b1572a2f60d2438affd26a2465717754
Paul W <zigloo99@gmail.com>
2012-04-09 09:35:07 -0500

    updated home page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,8 +45,8 @@ end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-  Factory.factories.clear
-  Dir[Rails.root.join("spec/factories/**/*.rb")].each{|f| load f}
+  # Factory.factories.clear
+  # Dir[Rails.root.join("spec/factories/**/*.rb")].each{|f| load f}
   
   #reloading routes
   Paulwanless::Application.reload_routes!
