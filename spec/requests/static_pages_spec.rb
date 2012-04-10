0ed841ec8c0a651a1afc758ba4c9d77893cffc8f
Paul W <zigloo99@gmail.com>
2012-04-09 22:45:50 -0500

    added footer with adjustable time

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,14 @@ describe "StaticPages" do
   
   describe "Home" do
     
-    it "should have the content 'Paul Wanless" do
+    it "should have the h1 'Paul Wanless" do
       visit root_path
-      page.should have_content('Paul Wanless')
+      page.should have_selector('h1', :text => 'Paul Wanless')
+    end
+    
+    it "should have the title 'Welcome'" do
+      visit root_path
+      page.should have_selector('title', :text => 'Paul Wanless | Welcome')
     end
   
   end
@@ -18,13 +23,23 @@ describe "StaticPages" do
       page.should have_content('Bio')
     end
     
+    it "should have the title 'Bio'" do
+      visit bio_path
+      page.should have_selector('title', :text => 'Paul Wanless | Bio')
+    end
+    
   end
   
   describe "Work_Experience" do
     
-    it "should have the content 'Work Experience'" do
+    it "should have the content 'Work Portfolio'" do
       visit work_experience_path
-      page.should have_content('Work Experience')
+      page.should have_content('Work Portfolio')
+    end
+    
+    it "should have the title 'Work Portfolio'" do
+      visit work_experience_path
+      page.should have_selector('title', :text => 'Paul Wanless | Work Portfolio')
     end
     
   end
@@ -32,9 +47,15 @@ describe "StaticPages" do
   describe "Work_Tools" do
     
     it "should have the content 'Engineering Tools'" do
-      visit work_experience_path
+      visit work_tools_path
       page.should have_content('Engineering Tools')
     end
+    
+    it "should have the title 'Engineering Tools'" do
+      visit bio_path
+      page.should have_selector('title', :text => 'Paul Wanless | Engineering Tools')
+    end
+    
     
   end
   
