bec8b634dd66248cbb9305572d295af97a4c93e4
Paul W <zigloo99@gmail.com>
2012-05-17 01:21:00 -0500

    set-up Gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Paulwanless::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  config.action_mailer.default_url_options = { :host => 'paulwanless.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  
+  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
