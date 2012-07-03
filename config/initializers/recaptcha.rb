aa1444575dd2109cf1975e7dfdbbe2173c8336f0
Paul W <zigloo99@gmail.com>
2012-07-03 18:29:46 -0500

    removed recaptch.rb content

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
-Recaptcha.configure do |config|
-  config.public_key  = RECAPTCHA_PUBLIC_KEY
-  config.private_key = RECAPTCHA_PRIVATE_KEY
-  config.proxy = 'http://myproxy.com.au:8080'
-end
+# Recaptcha.configure do |config|
+#   config.public_key  = RECAPTCHA_PUBLIC_KEY
+#   config.private_key = RECAPTCHA_PRIVATE_KEY
+#   config.proxy = 'http://myproxy.com.au:8080'
+# end
