c463ef0c3fb8cf5620151411612a9c8dd121d128
Paul W <zigloo99@gmail.com>
2012-07-04 15:27:51 -0500

    Added recaptcha gem

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
-# Recaptcha.configure do |config|
-#   config.public_key  = RECAPTCHA_PUBLIC_KEY
-#   config.private_key = RECAPTCHA_PRIVATE_KEY
-#   config.proxy = 'http://myproxy.com.au:8080'
-# end
+Recaptcha.configure do |config|
+  config.public_key  = '6LdCj9MSAAAAACenIOEOMvd0S90OvgI_RsusFl6m'
+  config.private_key = 'RECAPTCHA_PRIVATE_KEY'
+  config.proxy = 'http://myproxy.com.au:8080'
+end
