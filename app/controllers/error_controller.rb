187613fbae16caa1c8db9be8771d174129cff217
Paul W <zigloo99@gmail.com>
2012-07-05 01:19:43 -0500

    Added error controller

---
+++ app/controllers/error_controller.rb
@@ -0,0 +1,13 @@
+class ErrorController < ApplicationController
+  
+  def not_found
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
+      format.xml  { head :not_found }
+      format.any  { head :not_found }
+    end
+    
+    # render :text => 'Not Found', :status => '404'
+    # raise ActionController::RoutingError.new('Not Found')
+  end
+end
