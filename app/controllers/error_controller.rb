f1a09d6741bd862fc624913b6712c4ccf6b78f32
Paul W <zigloo99@gmail.com>
2012-07-06 02:30:57 -0500

    Added fancybox jQuery for PDF viewing

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -2,12 +2,15 @@ class ErrorController < ApplicationController
   
   def not_found
     respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
+      format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found,  }
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
     
     # render :text => 'Not Found', :status => '404'
-    # raise ActionController::RoutingError.new('Not Found')
+    
+    # def not_found
+    #   raise ActionController::RoutingError.new('Not Found')
+    # end
   end
 end
