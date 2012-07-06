f58b5c0a6571d185675791bf8d96dff49e975e45
Paul W <zigloo99@gmail.com>
2012-07-06 05:58:38 -0500

    fixed error controller

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -2,7 +2,7 @@ class ErrorController < ApplicationController
   
   def not_found
     respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found,  }
+      format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
